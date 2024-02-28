4970a68e8048ae9318fb199e5abd2762903bef7d
Christian <124cayala@frhs.com>
2024-02-28 08:36:39 -0500

    lesson 7 started

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 1,
             product: 'Socks',
             description: 'They are socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
             inventory: 100,
-            onSale: true
+            onSale: true,
+            details: ['50% Cotton', '30% Wool', '20% Polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
+            sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge']
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
